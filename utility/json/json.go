8acecc170228423fc80aed8b4fec0566c9366e03
CLOUDERHEM <78064276+CLOUDERHEM@users.noreply.github.com>
2024-09-18 11:25:58 +0000

    refactor: remove unnecessary err

--- utility/json/json.go
+++ utility/json/json.go
@@ -2,7 +2,7 @@ package mjson
 
 import "github.com/tidwall/gjson"
 
-func ListKeysAndValues(json []byte) (keys []string, values []string, err error) {
+func ListKeysAndValues(json []byte) (keys []string, values []string) {
 	r := gjson.Parse(string(json))
 	r.ForEach(func(key, value gjson.Result) bool {
 		keys = append(keys, key.String())
