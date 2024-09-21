28f3027e703863e6a94c8be860c4ab79a5858bff
CLOUDERHEM <cloudac@qq.com>
2024-09-22 00:16:46 +0800

    refactor: kv process

--- utility/parse/kv.go
+++ utility/parse/kv.go
@@ -1,8 +1,27 @@
 package parse
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
+
+type KV struct {
+	K string
+	V string
+}
 
 func GetValueByKey(str, key string) string {
+	values := GetKeyValues(str)
+	for i := range values {
+		if values[i].K == key {
+			return values[i].V
+		}
+	}
+	return ""
+}
+
+func GetKeyValues(str string) []KV {
+	var result []KV
 	kvs := strings.Split(str, ";")
 	for i := range kvs {
 		kv := strings.Split(kvs[i], "=")
@@ -11,9 +30,24 @@ func GetValueByKey(str, key string) string {
 		}
 		k := strings.TrimSpace(kv[0])
 		v := strings.Join(kv[1:], "=")
-		if k == key {
-			return v
+		result = append(result, KV{k, v})
+	}
+	return result
+}
+
+// TidyKvs use the not empty v if s has same ks,
+// use the last one if multi vs
+func TidyKvs(s string) string {
+	kvs := GetKeyValues(s)
+	mp := make(map[string]string)
+	for i := range kvs {
+		if mp[kvs[i].K] == "" || mp[kvs[i].K] == "\"\"" {
+			mp[kvs[i].K] = kvs[i].V
 		}
 	}
-	return ""
+	var lines []string
+	for k, v := range mp {
+		lines = append(lines, fmt.Sprintf("%s=%s", k, v))
+	}
+	return strings.Join(lines, ";")
 }
