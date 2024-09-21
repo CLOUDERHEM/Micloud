c60ab823e8b27d89aee05548eaef0cfd2f45d893
CLOUDERHEM <cloudac@qq.com>
2024-09-22 00:38:07 +0800

    fix: cannot create cookie file

--- authorizer/cookie/cookie.go
+++ authorizer/cookie/cookie.go
@@ -3,19 +3,24 @@ package cookie
 import (
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func getCookieFromFile(cookiePath string) string {
 	data, err := os.ReadFile(cookiePath)
 	if os.IsNotExist(err) {
-		err := os.WriteFile(cookiePath, []byte(""), 0644)
+		err := os.MkdirAll(filepath.Dir(cookiePath), os.ModePerm)
 		if err != nil {
-			log.Printf("can not create cookie file, path: %v, err: %v", cookiePath, err)
+			log.Fatalf("cannot mkdir for cookie path, path: %v, err: %v", cookiePath, err)
+		}
+		err = os.WriteFile(cookiePath, []byte(""), 0644)
+		if err != nil {
+			log.Fatalf("can not create cookie file, path: %v, err: %v", cookiePath, err)
 			return ""
 		}
 	} else if err != nil {
-		log.Printf("can not read cookie file, path: %v, err: %v", cookiePath, err)
+		log.Fatalf("can not read cookie file, path: %v, err: %v", cookiePath, err)
 		return ""
 	}
 	return strings.TrimSpace(string(data))
