399b7ff604d656046ddb7ef3ae557fd86ecb8d8d
CLOUDERHEM <cloudac@qq.com>
2024-09-22 23:18:15 +0800

    fix: err data type

--- micloud/gallery/timeline/model.go
+++ micloud/gallery/timeline/model.go
@@ -2,6 +2,6 @@ package timeline
 
 type Timeline struct {
 	// eg: 20240101 : 1
-	DayCount  map[string]int `json:"dayCount"`
-	IndexHash int64          `json:"indexHash"`
+	DayCount  map[int]int `json:"dayCount"`
+	IndexHash int64       `json:"indexHash"`
 }
