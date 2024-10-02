08ac5bd5efb1dd26c8fb42950f390ceece088ee5
CLOUDERHEM <cloudac@qq.com>
2024-10-02 12:40:41 +0800

    fix: do not add if err

--- utility/parallel/parallel.go
+++ utility/parallel/parallel.go
@@ -30,10 +30,11 @@ func DoParallel[In any, Out any](ins []In, exec func(In) (Out, error)) ([]Out, [
 				eLock.Lock()
 				e = append(e, ErrOut[In]{i, err})
 				eLock.Unlock()
+			} else {
+				sLock.Lock()
+				s = append(s, out)
+				sLock.Unlock()
 			}
-			sLock.Lock()
-			s = append(s, out)
-			sLock.Unlock()
 		}(in)
 	}
 
